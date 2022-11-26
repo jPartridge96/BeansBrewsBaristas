91cd6cd9482dfff7eb2f23eda79a0f4f1d18cece
Jordan Partridge <99056037+jPartridge96@users.noreply.github.com>
2022-11-26 14:35:53 -0500

    AudioManager now supports Songs and SoundEffects

--- BeansBrewsBaristas/Managers/AudioManager.cs
+++ BeansBrewsBaristas/Managers/AudioManager.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.VisualBasic.Devices;
-using Microsoft.Xna.Framework.Audio;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
 
 namespace BeansBrewsBaristas.Managers
 {
@@ -14,11 +12,18 @@ namespace BeansBrewsBaristas.Managers
         {
             _soundLibrary = new Dictionary<string, SoundEffect>()
             {
-                // {"PascalCase", LoadSound("camelCase") }
                 {"MenuTheme", LoadSound("Theme1") },
                 {"Theme2", LoadSound("Theme2") }
             };
+            _songLibrary = new Dictionary<string, Song>()
+            {
+                {"MenuTheme", LoadSong("Theme1") },
+                {"Theme2", LoadSong("Theme2") }
+            }; // SongCollection, MediaLibrary exist - useful?
+
+            MediaPlayer.IsRepeating = true;
         }
+        
 
         private static AudioManager _instance;
         public static AudioManager GetInstance()
@@ -33,25 +38,63 @@ namespace BeansBrewsBaristas.Managers
         /// </summary>
         /// <param name="filePath">Path to sound file</param>
         /// <returns>Returns SoundEffect if loaded successfully</returns>
+        /// <exception cref="ContentLoadException">Thrown if no SoundEffect is found with name</exception>
         public static SoundEffect LoadSound(string filePath)
         {
-            return Global.GameManager.Content.Load<SoundEffect>($"Music/{filePath}");
+            try
+            {
+                return Global.GameManager.Content.Load<SoundEffect>($"SoundEffects\\{filePath}");
+            }
+            catch
+            {
+                throw new ContentLoadException($"SoundEffect with name '{filePath}' not found.");
+            }
         }
 
         /// <summary>
-        /// Searches dictionary for sound using string; plays sound if found
+        /// Loads Song from file path into ContentManager
+        /// </summary>
+        /// <param name="filePath">Path to song file</param>
+        /// <returns>Returns Song if loaded successfully</returns>
+        /// <exception cref="ContentLoadException">Thrown if no Song is found with name</exception>
+        public static Song LoadSong(string filePath)
+        {
+            try
+            {
+                return Global.GameManager.Content.Load<Song>($"Songs\\{filePath}");
+            }
+            catch
+            {
+                throw new ContentLoadException($"Song with name '{filePath}' not found.");
+            }
+        }
+
+        /// <summary>
+        /// Searches dictionary for SoundEffect using string; plays SoundEffect if found
         /// </summary>
         /// <param name="sound">Name of SoundEffect</param>
-        /// <exception cref="Exception">Thrown if no sound is found with name</exception>
+        /// <exception cref="Exception">Thrown if no SoundEffect is found with name</exception>
         public static void PlaySound(string sound)
         {
             if (!_soundLibrary.TryGetValue(sound, out SoundEffect soundEffect))
-                throw new Exception($"SoundEffect with name '{sound} not found.'");
+                throw new Exception($"SoundEffect with name '{sound}' not found.");
             soundEffect.Play();
         }
 
+        /// <summary>
+        /// Searches dictionary for Song using string; plays Song if found
+        /// </summary>
+        /// <param name="songName">Name of Song</param>
+        /// <exception cref="Exception">Thrown if no Song is found with name</exception>
+        public static void PlaySong(string songName)
+        {
+            if (!_songLibrary.TryGetValue(songName, out Song song))
+                throw new Exception($"Song with name '{songName}' not found.");
+            MediaPlayer.Play(song);
+        }
 
         public static Dictionary<string, SoundEffect> _soundLibrary;
+        public static Dictionary<string, Song> _songLibrary;
     }
 }
 
--- BeansBrewsBaristas/Program.cs
+++ BeansBrewsBaristas/Program.cs
@@ -13,6 +13,12 @@
  *      Jordan Partridge -             : Added Debug class to assist in development
  *                       -             : Created CustomerManager to create and destroy Customers
  *                       -             : Removed Managers from Global; transitioned to singletons
+ *           Thomas Heal - Nov 15, 2022: Refactored AnimatedSprite with a working version of frameIndexes
+ *                       - Nov 16, 2022: Customer is now represented by an AnimatedSprite instead of a ball texture
+ *      Jordan Partridge - Nov 18, 2022: Refactored Customer and AnimatedSprite; removed debug code from GameManager
+ *                       -             : Customer will stop animating when they it has finished moving to target position
+ *           Thomas Heal - Nov 19, 2022: Added cafe environment art to Scene 1
+ *      Jordan Partridge - Nov 26, 2022: _
  */
 
 using var game = new BeansBrewsBaristas.GameManager();
